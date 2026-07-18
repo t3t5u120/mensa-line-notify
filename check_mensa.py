459c86e7c6cdd4281611c821c0d592ba5220716b
t3t5u120 <soulwarpjunk@gmail.com>
2026-07-19 00:02:00 +0900

    Update check_mensa.py

--- check_mensa.py
+++ check_mensa.py
@@ -1,4 +1,7 @@
 import hashlib
+import os
+import smtplib
+from email.message import EmailMessage
 from pathlib import Path
 
 import requests
@@ -27,6 +30,24 @@ else:
 if current_hash == old_hash:
     print("変更なし")
 else:
-    print("★★ MENSAページが更新されました ★★")
+    print("** MENSAページが更新されました **")
+
+    sender = os.environ["EMAIL_ADDRESS"]
+    password = os.environ["EMAIL_PASSWORD"]
+
+    msg = EmailMessage()
+    msg["Subject"] = "【MENSA】ページが更新されました"
+    msg["From"] = sender
+    msg["To"] = sender
+
+    msg.set_content(
+        "MENSAの受験案内ページが更新されました。\n\n"
+        "https://mensa.jp/exam/"
+    )
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(sender, password)
+        smtp.send_message(msg)
+
     HASH_FILE.write_text(current_hash)
 
